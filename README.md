2faaa9f0476908b53686e98f3552370021fbedcb
lds133 <lds133@gmail.com>
2025-06-07 08:45:35 +0200

    Moving to FFMPEG

--- README.md
+++ README.md
@@ -83,4 +83,103 @@ journalctl -u jukebox
 ```
 
 
+#### MPD
+
+```
+sudo apt-get install mpd 
+
+sudo nano /etc/mpd.conf
+
+sudo systemctl enable mpd
+sudo systemctl start mpd
+
+
+```
+
+/var/lib/mpd/music
+
+
+
+```
+pip install python-mpd2
+```
+https://github.com/Mic92/python-mpd2
+
+https://python-mpd2.readthedocs.io/en/latest/index.html
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------
+
+
+https://frequence3.net-radio.fr/frequence3-256.mp3
+
+```
+sudo apt update
+sudo apt install ffmpeg
+
+sudo apt install alsa-utils
+
+
+sudo usermod -aG audio $USER
+
+
+dima@ivan-pc:~$ aplay -l
+**** List of PLAYBACK Hardware Devices ****
+card 0: PCH [HDA Intel PCH], device 0: ALC3246 Analog [ALC3246 Analog]
+  Subdevices: 0/1
+  Subdevice #0: subdevice #0
+card 0: PCH [HDA Intel PCH], device 3: HDMI 0 [HDMI 0]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+card 0: PCH [HDA Intel PCH], device 7: HDMI 1 [HDMI 1]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+card 0: PCH [HDA Intel PCH], device 8: HDMI 2 [HDMI 2]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+dima@ivan-pc:~$
+
+speaker-test -D hw:0,0 -c 2 -t wav
+
+ffmpeg -i https://frequence3.net-radio.fr/frequence3-256.mp3 -f wav -acodec pcm_s16le -ar 44100 -ac 2 - | aplay -f cd -D hw:0,0
+
+
+
+
+
+
+
+nano ~/.asoundrc
+
+pcm.!default {
+    type hw
+    card 0
+    device 0
+}
+
+ctl.!default {
+    type hw
+    card 0
+}
+
+
+
+ffmpeg -i https://frequence3.net-radio.fr/frequence3-256.mp3 -f wav -acodec pcm_s16le -ar 44100 -ac 2 - | aplay -f cd
+
+
+
+amixer set Master 10%-
+amixer set Master 10%+
+
+amixer set Master 10%
+
+```
 
--- player.py
+++ player.py
@@ -1,43 +1,140 @@
-import vlc
+
 import time
+from threading import Thread, Event
+import subprocess
+
 
 
 
 class Player:
 
-    def __init__(self, file_m3u ):
-        instance = vlc.Instance()
-        self.player = instance.media_list_player_new()
-        media = instance.media_list_new([file_m3u])
-        self.player.set_media_list(media)
+
+    CMD_FFMPEG = [ "ffmpeg","-i","https://frequence3.net-radio.fr/frequence3-256.mp3","-f","wav","-acodec","pcm_s16le","-ar","44100","-ac","2","-"]
+    CMD_APLAY = ["aplay","-f","cd"]
+    
+    VOLCMDTEMPLATE = "amixer set Master %i%%"
+
+    def __init__(self):
+    
+        self.thread = None
+        self.p_ffmpeg = None
+        self.p_aplay = None
+        self.threadstopped = None
+        self.threadstarted = None
+        self.iskilling = False
         
-    def Play(self):   
-        return self.player.play() 
         
-    def Stop(self):   
-        return self.player.stop() 
-      
+    def Play(self):
+        if (self.IsPlaying):
+            self.Stop()
+            
+        self.iskilling = False
+        self.p_ffmpeg = None
+        self.p_aplay = None
+        self.threadstopped = Event()
+        self.threadstarted = Event()
+        self.thread = Thread(target = self.ThreadProc )
+        self.thread.start()
+        self.threadstarted.wait()
+        if not self.IsCmdRunning:
+            self.Stop()
+            return False
+        return True
+    
+    @property
+    def IsCmdRunning(self):
+        return self.p_ffmpeg and self.p_aplay
+        
+    def ThreadProc(self):
+        print(">>>","Thread start")
+        
+        try:
+            self.p_ffmpeg = subprocess.Popen(
+                    self.CMD_FFMPEG, 
+                    stdout=subprocess.PIPE,
+                    preexec_fn=os.setsid # detaches the subprocess from the terminal control group
+                    )
+        except Exception as e:
+            print(">>>","Error ffmpeg", str(e))
+            self.p_ffmpeg = None
+
+        try:
+            self.p_aplay = subprocess.Popen(self.CMD_APLAY, stdin=self.p_ffmpeg.stdout )
+        except Exception as e:
+            print(">>>","Error aplay", str(e))
+            self.p_aplay = None
+
+        self.threadstarted.set()
+        if (self.IsCmdRunning):
+            self.p_ffmpeg.wait()
+            self.p_aplay.wait()
+        self.threadstopped.set()
+        print(">>>","Thread stop")
+        
+        
+
+    def Stop(self):
+        if not self.IsPlaying:
+            print(">>>","Thread already stopped")
+            return True
+        if self.iskilling:
+            print(">>>","Thread kill in progress")
+            return True
+        print(">>>","Thread kill")
+        self.iskilling = True
+        if self.p_ffmpeg:
+            self.p_ffmpeg.kill() 
+        if self.p_aplay:
+            self.p_aplay.kill()
+        self.threadstopped.wait()
+        self.thread = None
+        self.threadstopped = None
+        self.threadstarted = None
+        self.p_fmpeg = None
+        self.p_aplay = None
+        self.iskilling = False
+        return True
+    
+    
+
+        
+        
+     
     def Next(self):
-        return self.player.next() 
+        pass
+        return True
         
     def ChangeVolume(self,dv):
-        value = self.player.get_media_player().audio_get_volume()    
-        value+=dv
-        if (value>100):
-            value=100
-        if (value<0):
-            value=0
-        self.player.get_media_player().audio_set_volume(value)
-        return value
+        cmd = self.VOLCMDTEMPLATE % dv
+        print("Run:",cmd)
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        process.wait()
+        return dv
         
     @property
     def IsPlaying(self):
-        return self.player.is_playing() != 0
+        return (self.thread!=None)
             
             
             
 if __name__ == "__main__":
 
-    box = Player("test.m3u")
+    box = Player()
+
     box.Play()
-    input("Press enter to continue...")
+    
+    print("----------------- SLEEP")
+    time.sleep(15)
+    
+    box.Stop()
+    
+    print("----------------- SLEEP")
+    time.sleep(15)
+
+    box.Play()
+    
+    print("----------------- SLEEP")
+    time.sleep(15)
+    
+    box.Stop()
+    
